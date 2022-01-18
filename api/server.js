5ecfa0c06ee0cce8749a4af8a8eb9e5f57d5a1ea
Justin Peisker <justinpeisker@gmail.com>
2022-01-18 11:09:35 -0800

    wip

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
-
+const carsRouter = require('./cars/cars-router')
 const server = express()
 
 server.use(express.json())
 
-server.use('*', (req, res) => {
+server.use('/api/cars', carsRouter)
+
+server.use('*', (req, res, next) => {
     next({status: 404, message: 'not found'})
 })
 
