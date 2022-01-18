bc85e90714fd18b89dce3891e0e3b1223b82425a
Justin Peisker <justinpeisker@gmail.com>
2022-01-18 10:43:18 -0800

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,16 @@
 // STRETCH
+const cars = [
+    {
+        vin: '11111111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 100000,
+        title: 'clean',
+        transmission: 'automatic',
+    }
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
